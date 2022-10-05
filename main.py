03afb010f9129da00ed24e28f4cfe6ab9aecaab3
victoryokirie <victoryokirie98@gmail.com>
2022-10-05 12:53:09 +0100

    calculator functions

---
+++ main.py
@@ -0,0 +1,16 @@
+# Calculator
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+def exponential(n1, n2):
+    return n1 ** n2
