f339e3b689094eb7bbe049fd22da5bca3bb6feb8
victoryokirie <victoryokirie98@gmail.com>
2022-10-05 13:06:09 +0100

    calculator function

--- main.py
+++ main.py
@@ -14,3 +14,21 @@ def divide(n1, n2):
 
 def exponential(n1, n2):
     return n1 ** n2
+
+operations ={
+    "+": add,
+    "-": subtract,
+    "/": divide,
+    "*": multiply,
+    "**": exponential
+}
+
+num1= int(input("What't the first number? "))
+num2= int(input("What't the second number? "))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol= input("Pick an operation from the line above..: ")
+calculator_function= operations[operation_symbol]
+calculator_function(num1, num2)
