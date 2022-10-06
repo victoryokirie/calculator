112dc31dc218e6f360c7c03e8c822970259a1861
victoryokirie <victoryokirie98@gmail.com>
2022-10-06 14:36:51 +0100

    recursive function

--- main.py
+++ main.py
@@ -23,12 +23,36 @@ operations ={
     "**": exponential
 }
 
-num1= int(input("What't the first number? "))
-num2= int(input("What't the second number? "))
+def calculator():
+    num1= int(input("What't the first number? "))
 
-for symbol in operations:
-    print(symbol)
+    for symbol in operations:
+        print(symbol)
 
-operation_symbol= input("Pick an operation from the line above..: ")
-calculator_function= operations[operation_symbol]
-calculator_function(num1, num2)
+    should_continue=True
+    
+    while should_continue:
+
+        operation_symbol= input("Pick an operation: ")
+
+        num2= int(input("What't the next number? "))
+
+        calculator_function= operations[operation_symbol]
+        answer=calculator_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        looping_condition=input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation or type 'exit' to exit the calculator: ")
+        if looping_condition == "y":
+            num1=answer
+        elif looping_condition == "n":
+            should_continue= False
+            calculator()
+        else:
+            should_continue= False
+            print("Calculator exited")
+
+calculator()
+
+
+
